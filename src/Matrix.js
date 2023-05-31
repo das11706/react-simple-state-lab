540ccf95e071edf909440c073fb9c8d151c598af
Daniel Suarez <suarez.daniel101@gmail.com>
2023-05-30 21:54:09 -0400

    Create Cell component and add constructor and render method. Create updateColor method

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 export default class Matrix extends Component {
   
   genRow = (vals) => {
+    // console.log(vals)
     return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
+    // console.log(this.props.values)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -16,6 +18,21 @@ export default class Matrix extends Component {
         {this.genMatrix()}
       </div>
     )
-  }
-  
+  } 
 }
+
+
+Matrix.defaultProps = {
+  values: [
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
+};
---
+++ src/cell.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      color: props.value,
+    };
+  };
+
+  updateColor = () => {
+    const newColor = this.state.color;
+    this.setState({
+      color: '#333',
+    });
+  };
+
+  render() {
+    return (
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.updateColor}></div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
